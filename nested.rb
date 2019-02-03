c7ed0b9fc001bdf7aa0458fccb10154843960ca3
Jessy Hu <jesshuy12@gmail.com>
2019-02-03 18:50:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -38,7 +39,7 @@ def alan_kay_is_known_for
         }
      }
      
-     programmer_hash[:alan_kay][]
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
